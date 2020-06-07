2ad2c49f256f5aedff5f6e1d352e2edffa7a7d91
Weverton Dini <51500957+wevertondiniz@users.noreply.github.com>
2020-06-07 12:37:03 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/wevertondiniz/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.wevertondiniz.git;
+
+/**
+ * @author PC
+ *Exemplo de integração com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
